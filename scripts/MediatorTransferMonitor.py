9aea204b12e9af0e2526f0da242ffab116fb083f
thetherington <thomas@hetheringtons.org>
2021-03-12 11:02:47 -0500

    updated new object with class inheritence to disocvery hostname via infocenter or insite

--- scripts/MediatorTransferMonitor.py
+++ scripts/MediatorTransferMonitor.py
@@ -6,7 +6,144 @@ import copy
 import requests
 
 
-class MediatorTransferMon:
+class findActiveTransferHost:
+    def __init__(self):
+
+        self.request_transfer_manager = {
+            "uniqueid": None,
+            "command": "TransferJobManager",
+            "noxsl": None,
+        }
+
+        self.request_services_params = {
+            "uniqueid": 0,
+            "command": "getservices",
+            "noxsl": None,
+        }
+
+        self.refresh_hostname_time = datetime.datetime.utcnow() - datetime.timedelta(minutes=15)
+
+        self.refresh_hostname()
+
+    def refresh_hostname(self):
+
+        try:
+
+            dt_delta = datetime.datetime.utcnow() - self.refresh_hostname_time
+
+            if dt_delta.total_seconds() > 300:
+
+                hostname = self.insite_lookup() if self.insite else self.infocenter_lookup()
+
+                if hostname:
+
+                    self.request_params["hostname"] = hostname
+                    self.refresh_hostname_time = datetime.datetime.utcnow()
+
+                    print("updated refresh", self.refresh_hostname_time)
+
+        except Exception:
+            pass
+
+    def infocenter_lookup(self):
+        def get_element(node, name):
+
+            try:
+                return node.getElementsByTagName(name)[0].firstChild.data
+
+            except Exception:
+                return None
+
+        active_hostname = None
+
+        with requests.Session() as http_session:
+
+            if self.logon:
+
+                # exit out of function if login failed
+                if not self.login(http_session):
+                    return None
+
+            try:
+
+                # get the uniqueid first for the info center host
+                resp = http_session.get(
+                    self.url,
+                    params=self.request_services_params,
+                    headers=self.headers,
+                    timeout=10.0,
+                )
+
+                doc = minidom.parseString(str(resp.text))
+
+                # iterate through a large list of service registration hosts
+                for service in doc.getElementsByTagName("ServiceReg"):
+
+                    # match to the init info center host
+                    if (
+                        get_element(service, "Host") == self.host
+                        and get_element(service, "Name") == "Turbine"
+                    ):
+
+                        self.request_transfer_manager["uniqueid"] = int(
+                            get_element(service, "UniqueID")
+                        )
+
+                        # get the active hostname from the job center
+                        resp_transfer_manager = http_session.get(
+                            self.url,
+                            params=self.request_transfer_manager,
+                            headers=self.headers,
+                            timeout=5.0,
+                        )
+
+                        doc_transfer_manager = minidom.parseString(str(resp_transfer_manager.text))
+
+                        for service_reg in doc_transfer_manager.getElementsByTagName(
+                            "ServiceRegistrationInfo"
+                        ):
+
+                            active_hostname = get_element(service_reg, "ActiveHostname")
+
+                        # exit iteration of all services
+                        break
+
+            except Exception as e:
+                print(e)
+
+            if self.logon:
+                self.logout(http_session)
+
+        return active_hostname
+
+    def insite_lookup(self):
+
+        query = {
+            "size": 1,
+            "query": {
+                "bool": {
+                    "must": [
+                        {"match_phrase": {"metricset.module": {"query": "mediator_transfers"}}},
+                        {"range": {"@timestamp": {"from": "now-5m", "to": "now"}}},
+                    ]
+                }
+            },
+            "sort": [{"@timestamp": {"order": "desc"}}],
+        }
+
+        url = "http://{}:9200/log-metric-*/_search".format(self.insite)
+
+        try:
+
+            resp = requests.get(url, data=json.dumps(query), timeout=30.0).json()
+
+            return resp["hits"]["hits"][-1]["_source"]["host"]
+
+        except Exception:
+            return None
+
+
+class MediatorTransferMon(findActiveTransferHost):
     def __init__(self, **kwargs):
 
         self.host = None
@@ -21,7 +158,6 @@ class MediatorTransferMon:
         self.system_name = None
 
         self.request_params = {
-            # "uniqueid": 535880,
             "hostname": None,
             "name": "turbine",
             "command": "TransferJobManager",
@@ -81,31 +217,8 @@ class MediatorTransferMon:
 
         self.url = "http://{}:{}/{}".format(self.host, self.port, self.route)
 
-    def findhost(self):
-
-        query = {
-            "size": 1,
-            "query": {
-                "bool": {
-                    "must": [
-                        {"match_phrase": {"metricset.module": {"query": "mediator_transfers"}}},
-                        {"range": {"@timestamp": {"from": "now-5m", "to": "now"}}},
-                    ]
-                }
-            },
-            "sort": [{"@timestamp": {"order": "desc"}}],
-        }
-
-        url = "http://{}:9200/log-metric-*/_search".format(self.insite)
-
-        try:
-
-            resp = requests.get(url, data=json.dumps(query), timeout=30.0).json()
-
-            self.request_params["hostname"] = resp["hits"]["hits"][-1]["_source"]["host"]
-
-        except Exception:
-            return None
+        if self.findhostname:
+            findActiveTransferHost.__init__(self)
 
     def login(self, http_session):
 
@@ -149,9 +262,6 @@ class MediatorTransferMon:
 
     def fetch(self):
 
-        if self.findhostname:
-            self.findhost()
-
         try:
 
             with requests.Session() as http_session:
@@ -188,6 +298,10 @@ class MediatorTransferMon:
         documents = []
 
         if not self.offline_doc:
+
+            if self.findhostname:
+
+                self.refresh_hostname()
 
             doc = self.fetch()
 
@@ -291,19 +405,25 @@ class MediatorTransferMon:
 def main():
 
     params = {
-        "host": "10.127.17.94",
+        "host": "10.127.3.80",
         "port": "8080",
         "insite": None,
         "theshold": None,
-        "hostname": "ip-10-127-17-94",
+        "hostname": "_auto_",
         "login": {"user": "evertz", "pass": "pharos1"},
         "system_name": "MAM_Production",
-        # "local_file": "multiple_Executors.xml",
+        # "local_file": "response.xml",
     }
 
     transfer = MediatorTransferMon(**params)
 
-    print(json.dumps(transfer.process(), indent=1))
+    inputQuit = False
+
+    while inputQuit is not "q":
+
+        print(json.dumps(transfer.process(), indent=1))
+
+        inputQuit = input("\nType q to quit or just hit enter: ")
 
 
 if __name__ == "__main__":
